f5ece33eb1f535df3ce7cb8e588738b1e19fe37d
jfrisina <jaki.frisina@gmail.com>
2024-05-21 14:06:58 -0700

    standardize button width, remove butttons from This Week section

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,7 +1,28 @@
-// add width rules to buttons using javascript. grab class "width"
+// TODAY ------------------------------------------------------------
+
+
+// BUTTONS ------------------------------------------------------------
+// standardize button width using javascript
+const buttons = document.getElementsByClassName("width");
+
+for (i = 0; i < buttons.length; i++) {
+    buttons[i].style.width = "14rem";
+    
+}
+
+
+
+// CARDS ------------------------------------------------------------
+const cards = document.getElementsByClassName("card");
+const cardTitles = document.getElementsByClassName("card-title");
+
+// set fixed width on card class and bold card titles
+ for (i = 0; i < cards.length; i++) {
+    cards[i].style.width = "10rem";
+    cardTitles[i].style.fontWeight = "600";
+ }
 
 // add background color to card headers
 
 // active indicator for which day is today on This Week
 
-// bold all of the class card-title
