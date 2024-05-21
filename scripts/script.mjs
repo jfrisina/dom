5a4a3ca791f7f0dc274d28642eec4cd08e825437
jfrisina <jaki.frisina@gmail.com>
2024-05-21 15:54:09 -0700

    add names to today's list

--- scripts/script.mjs
+++ scripts/script.mjs
@@ -1,6 +1,11 @@
 // TODAY ------------------------------------------------------------
 
 
+
+
+
+
+
 // BUTTONS ------------------------------------------------------------
 // standardize button width using javascript
 const buttons = document.getElementsByClassName("width");
@@ -10,9 +15,7 @@ for (i = 0; i < buttons.length; i++) {
     
 }
 
-
-
-// CARDS ------------------------------------------------------------
+// THIS WEEK ------------------------------------------------------------
 const cards = document.getElementsByClassName("card");
 const cardTitles = document.getElementsByClassName("card-title");
 
@@ -20,9 +23,39 @@ const cardTitles = document.getElementsByClassName("card-title");
  for (i = 0; i < cards.length; i++) {
     cards[i].style.width = "10rem";
     cardTitles[i].style.fontWeight = "600";
+    cards[i].style.minHeight = "10rem";
  }
 
 // add background color to card headers
 
+
+
 // active indicator for which day is today on This Week
+
+// FORM ------------------------------------------------------------
+// set variables
+const submitButton = document.getElementById("btn-submit");
+const todayList = document.getElementById("today-list");
+const staffInfo = document.querySelectorAll("input");
+const staffName = staffInfo[0].value;
+const startDate = staffInfo[1].value;
+const endDate = staffInfo[2].value;
+const today = new Date().toISOString().substring(0,10);
+
+
+// attach event listener to button
+submitButton.addEventListener("click", addPerson);
+
+// add name to Today's list
+function addPerson() {
+    if (startDate === today) {
+        let personItem = document.createElement("li");
+        personItem.textContent = staffName;
+        todayList.appendChild(personItem);
+        staffInfo.forEach(input => {
+            input.value = "";
+        })
+    }            
+};
+
 
