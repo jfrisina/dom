abd722537d1514f8397d9cd7248b28d821d0f96c
jfrisina <jaki.frisina@gmail.com>
2024-05-21 13:29:54 -0700

    add sections today, buttons, this week, and form

---
+++ scripts/script.mjs
@@ -0,0 +1,7 @@
+// add width rules to buttons using javascript. grab class "width"
+
+// add background color to card headers
+
+// active indicator for which day is today on This Week
+
+// bold all of the class card-title
